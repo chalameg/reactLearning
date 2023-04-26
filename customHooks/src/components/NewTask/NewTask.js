66de635fa1f3208268ef66a2367e431250133a64
unknown <chalamegersa5@gmail.com>
2023-04-26 11:50:14 +0300

    customHooks updated

--- customHooks/src/components/NewTask/NewTask.js
+++ customHooks/src/components/NewTask/NewTask.js
@@ -2,40 +2,24 @@ import { useState } from 'react';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
+import useHttp from '../../hooks/use-http';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const {isLoading, error, sendRequest: sendTasksRequest} = useHttp();
 
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://flutter-chat-ba300-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
+  const createTask = (taskText, data) => {
       const generatedId = data.name; // firebase-specific => "name" contains generated id
       const createdTask = { id: generatedId, text: taskText };
 
       props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+  }
+  const enterTaskHandler = async (taskText) => {
+
+    sendTasksRequest({url: 'https://flutter-chat-ba300-default-rtdb.firebaseio.com/tasks.json',method: 'POST',
+    body: JSON.stringify({ text: taskText }),
+    headers: {
+      'Content-Type': 'application/json',
+    }}, createTask.bind(null, taskText))
   };
 
   return (
