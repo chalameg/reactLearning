4ea6c095c359c6c8b1e89a5ceed7865139bbd7f9
chalameg <chalamegersa5@gmail.com>
2023-06-17 11:13:40 +0300

    nextjs course added

--- authentication/frontend/src/App.js
+++ authentication/frontend/src/App.js
@@ -13,13 +13,19 @@ import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
-import AuthenticationPage from './pages/Authentication';
+import AuthenticationPage, {
+  action as authAction,
+} from './pages/Authentication';
+import { action as logoutAction } from './pages/Logout';
+import { checkAuthLoader, tokenLoader } from './util/auth';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id: 'root',
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
       {
@@ -45,6 +51,7 @@ const router = createBrowserRouter([
                 path: 'edit',
                 element: <EditEventPage />,
                 action: manipulateEventAction,
+                loader: checkAuthLoader,
               },
             ],
           },
@@ -52,8 +59,14 @@ const router = createBrowserRouter([
             path: 'new',
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader: checkAuthLoader,
           },
         ],
+      },
+      {
+        path: 'auth',
+        element: <AuthenticationPage />,
+        action: authAction,
       },
       {
         path: 'newsletter',
@@ -61,9 +74,9 @@ const router = createBrowserRouter([
         action: newsletterAction,
       },
       {
-        path: 'auth',
-        element: <AuthenticationPage />,
-      }
+        path: 'logout',
+        action: logoutAction,
+      },
     ],
   },
 ]);
